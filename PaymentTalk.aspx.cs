212dc23183acdfe7e6947fe71afefd5bbfd1fd44
Janway <janway@global-opto.com>
2013-09-11 13:20:30 +0800

    change taxSum,taxClearance,grandTotal
    parameters

--- PaymentTalk.aspx.cs
+++ PaymentTalk.aspx.cs
@@ -37,15 +37,17 @@ namespace WowkoolMini
             string _res = string.Empty;
             string apiKey = "V1QpQ87gCx94Mpxz3ARk23VaSRaNIWE6kI40HEUxF";
             string _userId = "janway@global-opto.com";
-            string _orderNumber = "99293213-122";
-            string[] _itemNames = {"充電器","耳機","運費","生日蛋糕","運費"};
+            string _orderNumber = "100000266";
+            string[] _itemNames = {"刷子","洗衣精","運費","生日蛋糕","運費"};
             string[] _itemId = { "PROD_020202", "PROD_020201", "FEE_000002", "PROD_030302", "FEE_000002" };
-            string[] _vendorId = { "shop123", "shop123", "shop123","66shopping","66shopping" };
-            string[] _vendorName = { "商店123", "商店123", "商店123", "66小舖", "66小舖" };
+            string[] _vendorId = { "sky000", "sky000", "sky000", "66shopping", "66shopping" };
+            string[] _vendorName = { "天天小站", "天天小站", "天天小站", "66小舖", "66小舖" };
+            string[] _flagShippingFee = { "N", "N", "Y", "N", "Y" };
+            string[] _trackingCode = { "", "", "SH0001", "", "SH0002" };
             int[] _qtys = {1,1,1,1,1};
-            double[] _amts = {890,1000,0,670,0};
+            double[] _amts = {60,199,10,670,0};
 
-            double _tax = 0,_amountSum=0;
+            double _taxSum = 0, _amountSum = 0, _taxClearence = 0;
 
             string _buyerId =  "janway@global-opto.com";
             string _buyerName =  "周杰倫";
@@ -75,12 +77,15 @@ namespace WowkoolMini
                         .Append("&vendorName=").Append(Server.UrlEncode(_vendorName[i]))
                         .Append("&itemId=").Append(Server.UrlEncode(_itemId[i]))
                         .Append("&itemName=").Append(Server.UrlEncode(_itemNames[i]))
+                        .Append("&flagShippingFee=").Append(Server.UrlEncode(_flagShippingFee[i]))
+                        .Append("&trackingCode=").Append(Server.UrlEncode(_trackingCode[i]))
                         .Append("&qty=").Append(_qtys[i])
-                        .Append("&amt=").Append(_amts[i])
-                        .Append("&tax=").Append(_qtys[i]*_amts[i]*0.05); //5% tax for example
+                        .Append("&amt=").Append(_amts[i]);
                 _amountSum += _qtys[i] * _amts[i];
             }
-            postData.Append("&amountSum=").Append(_amountSum)
+            postData.Append("&grandTotal=").Append(_amountSum+_taxSum+_taxClearence)
+                    .Append("&taxSum=").Append(_taxSum)
+                    .Append("&taxClearance=").Append(_taxClearence)
                     .Append("&rcvrName=").Append(Server.UrlEncode(_rcvrName))
                     .Append("&rcvrPhoneNo=").Append(Server.UrlEncode(_rcvrPhoneNo))
                     .Append("&rcvrAddress=").Append(Server.UrlEncode(_rcvrAddress))
